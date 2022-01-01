e45acd6094437604efc4d8d38308d5edb9a395d4
Andriy <andriy.pidhorny@gmail.com>
2022-01-01 21:43:20 +0200

    add post-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import PostList from './components/PostList';
+import './styles/style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React.js</h1>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'JavaScript - 1', body: 'lorem spsumv omega tetra stu erusm' },
+        { id: 2, title: 'JavaScript - 2', body: 'lorem spsumv omega tetra stu erusm' },
+        { id: 3, title: 'JavaScript - 3', body: 'lorem spsumv omega tetra stu erusm' },
+    ]);
+
+    return (
+        <div className='App'>
+            <PostList posts={posts} title={'Posts List'} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PostItem = (props) => {
+    return (
+        <div className='post'>
+            <div className='post__content'>
+                <div>
+                    <strong>
+                        {props.post.id}. {props.post.title}
+                    </strong>
+                </div>
+                <div>{props.post.body}</div>
+            </div>
+            <div className='pots__btns'>
+                <button className='btn--delete'>Delete</button>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
